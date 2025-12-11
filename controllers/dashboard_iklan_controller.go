666a356ba117d25b2bcf0e8b450cf0779b40247e
rofitls <rofituluss2@gmail.com>
2025-12-11 21:48:16 +0700

    Get Penjualan Iklan

--- controllers/dashboard_iklan_controller.go
+++ controllers/dashboard_iklan_controller.go
@@ -12,7 +12,72 @@ import (
 )
 
 func GetDashboardIklanPenjualanIklan(c *gin.Context) {
+	var result dto.ResponseHeaderDashboardIklan
+	input, errBind := utils.BindJSON[dto.RequestDashboardIklan](c)
+	if errBind != nil {
+		utils.Error(c, http.StatusBadRequest, "Invalid input", errBind.Error())
+		return
+	}
+	db := config.DB
+	layout := "2006-01-02"
 
+	// convert string to time.Time
+	dateFrom, err1 := time.Parse(layout, input.DateFrom)
+	dateTo, err2 := time.Parse(layout, input.DateTo)
+	if err1 != nil || err2 != nil {
+		utils.Error(c, http.StatusBadRequest, "Invalid date format, use YYYY-MM-DD", nil)
+		return
+	}
+
+	var current, previous float64
+
+	// total penjualan periode sekarang
+	db.Raw(`
+    SELECT COALESCE(SUM(omzet_penjualan), 0)
+    FROM shopee_data_upload_iklan_details
+    WHERE store_id = ? AND tanggal BETWEEN ? AND ?
+`, input.StoreId, dateFrom, dateTo).Scan(&current)
+
+	// cari range periode sebelumnya
+	days := int(dateTo.Sub(dateFrom).Hours()/24) + 1
+	periodBeforeFrom := dateFrom.AddDate(0, 0, -days)
+	periodBeforeTo := dateTo.AddDate(0, 0, -days)
+
+	// total periode sebelumnya
+	db.Raw(`
+    SELECT COALESCE(SUM(omzet_penjualan), 0)
+    FROM shopee_data_upload_iklan_details
+    WHERE store_id = ? AND tanggal BETWEEN ? AND ?
+`, input.StoreId, periodBeforeFrom, periodBeforeTo).Scan(&previous)
+
+	// hitung persentase naik / turun
+	changePercent := 0.0
+	if previous > 0 {
+		changePercent = ((current - previous) / previous) * 100
+	}
+	if changePercent > 0 {
+		result.Trend = "Up"
+	} else if changePercent == 0 {
+		result.Trend = "Equal"
+	} else {
+		result.Trend = "Down"
+	}
+
+	result.Total = current
+	result.Percent = changePercent
+
+	var sparkline []dto.ResponseSparkline
+	// detail untuk sparkline
+	db.Raw(`
+	SELECT tanggal, SUM(omzet_penjualan) AS total
+	FROM shopee_data_upload_iklan_details
+	WHERE store_id = ? AND tanggal BETWEEN ? AND ?
+	GROUP BY tanggal
+	ORDER BY tanggal ASC;`, input.StoreId, dateFrom, dateTo).Scan(&sparkline)
+
+	result.Sparkline = sparkline
+
+	utils.Success(c, constant.DashboardIklanConst+constant.SuccessFetch, result)
 }
 
 func GetDashboardIklanBiayaIklan(c *gin.Context) {
